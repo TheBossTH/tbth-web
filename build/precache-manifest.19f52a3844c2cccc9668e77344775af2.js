73cc93b5e14c9baf92185a6236f79a64fbb60c62
Kittidet Cheavachirakul <fvgbhv14@gmail.com>
2020-09-08 22:36:42 +0700

    del build

---
+++ build/precache-manifest.19f52a3844c2cccc9668e77344775af2.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bc1b4589b3740d60dbe583ea2e6e7618",
+    "url": "/index.html"
+  },
+  {
+    "revision": "94a51537dedb6ee36f34",
+    "url": "/static/css/main.60107328.chunk.css"
+  },
+  {
+    "revision": "8cc750b47f4b243d30fe",
+    "url": "/static/js/2.7d6b9804.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7d6b9804.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "94a51537dedb6ee36f34",
+    "url": "/static/js/main.0a1ca4a3.chunk.js"
+  },
+  {
+    "revision": "4ec0938582e4597c48d8",
+    "url": "/static/js/runtime-main.fd67bc72.js"
+  },
+  {
+    "revision": "1db5f6d4edd1a93b3a4f80840976eddc",
+    "url": "/static/media/bg1.1db5f6d4.webp"
+  }
+]);
